3edde567d101e629281dd19f55473ef4eb819178
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-07-21 10:12:51 +0530

    codes

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const buttons = document.querySelectorAll("button");
-for (let button of buttons) {
-  button.addEventListener("click",changeColors);
-}
-
-const h2s = document.querySelectorAll("h2");
-for (let h2 of h2s) {
-  h2.addEventListener("click", changeColors);
-}
-
-function changeColors() {
-  this.style.backgroundColor = "red";
-  this.style.color = "white";
-  this.style.fontSize = "20px";
-}
+document.querySelector("input").addEventListener("keydown", function (e) {
+  console.log(e.code);
+});
